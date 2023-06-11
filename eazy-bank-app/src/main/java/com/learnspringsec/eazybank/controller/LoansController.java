803b4173fd8a661aa6ffc755c067d527f6f5465b
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-11 18:30:32 +0800

    add Balance and Loans Controller

---
+++ eazy-bank-app/src/main/java/com/learnspringsec/eazybank/controller/LoansController.java
@@ -0,0 +1,14 @@
+package com.learnspringsec.eazybank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/myLoans")
+public class LoansController {
+  
+  @GetMapping("/myLoans")
+  public String getAccountDetails() {
+    return "Here are the loans details from the DB";
+  }
+
+}
