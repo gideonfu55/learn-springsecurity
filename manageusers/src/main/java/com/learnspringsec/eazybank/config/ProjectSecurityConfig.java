3e294226bab87c2585058da34ee83221293808ab
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-12 09:02:27 +0800

    add users in config - using DefaultPasswordEncoder method

--- manageusers/src/main/java/com/learnspringsec/eazybank/config/ProjectSecurityConfig.java
+++ manageusers/src/main/java/com/learnspringsec/eazybank/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,6 +22,20 @@ public class ProjectSecurityConfig {
       .httpBasic(Customizer.withDefaults());
 
     return http.build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+    // Approach to use DefaultPasswordEncoder() method
+
+    UserDetails admin = User.withDefaultPasswordEncoder()
+      .username("admin")
+      .password("12345")
+      .roles("ADMIN")
+      .build();
+
+    return new InMemoryUserDetailsManager(admin);
   }
 
 }
