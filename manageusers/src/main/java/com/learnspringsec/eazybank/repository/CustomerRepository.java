43b9676eb4b12a53b404901941324ea33a0d3326
Gideon Fu <fuzhonglong@gmail.com>
2023-08-15 17:21:31 +0800

    update CustomerRepository

--- manageusers/src/main/java/com/learnspringsec/eazybank/repository/CustomerRepository.java
+++ manageusers/src/main/java/com/learnspringsec/eazybank/repository/CustomerRepository.java
@@ -3,7 +3,9 @@ package com.learnspringsec.eazybank.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.learnspringsec.eazybank.model.Customer;
+
 @Repository
-public interface CustomerRepository extends CrudRepository {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
   
 }
