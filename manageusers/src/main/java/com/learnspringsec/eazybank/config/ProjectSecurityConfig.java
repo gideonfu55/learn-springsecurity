278ac5f1dc44e20b37c05ecb4f1cea73f8c692c8
Gideon Fu <fuzhonglong@gmail.com>
2023-08-16 18:17:28 +0800

    updates

--- manageusers/src/main/java/com/learnspringsec/eazybank/config/ProjectSecurityConfig.java
+++ manageusers/src/main/java/com/learnspringsec/eazybank/config/ProjectSecurityConfig.java
@@ -1,15 +1,15 @@
 package com.learnspringsec.eazybank.config;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+// import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
--- manageusers/src/main/java/com/learnspringsec/eazybank/repository/CustomerRepository.java
+++ manageusers/src/main/java/com/learnspringsec/eazybank/repository/CustomerRepository.java
@@ -3,11 +3,9 @@ package com.learnspringsec.eazybank.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.learnspringsec.eazybank.model.Customer;
 
-@Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
   
   List<Customer> findByEmail(String email);
