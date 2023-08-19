d311813dfdaf3613138e4094a040cfd04c61a5f0
Gideon Fu <fuzhonglong@gmail.com>
2023-08-19 18:31:30 +0800

    add section 6 spring api - CORs and CSRF

---
+++ eazybank-webapp/section6-cors-csrf/src/main/java/com/learnspringsec/eazybank/controller/LoginController.java
@@ -0,0 +1,53 @@
+package com.learnspringsec.eazybank.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learnspringsec.eazybank.model.Customer;
+import com.learnspringsec.eazybank.repository.CustomerRepository;
+
+@RestController
+public class LoginController {
+  
+  @Autowired
+  private CustomerRepository customerRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  @PostMapping("/register")
+  public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+
+    Customer savedCustomer = null;
+    ResponseEntity<String> response = null;
+    
+    try {
+      // This is to encode the password before setting the password for customer:
+      String hashPwd = passwordEncoder.encode(customer.getPassword());
+
+      // Set password to the encoded password before saving it to the database:
+      customer.setPassword(hashPwd);
+
+      savedCustomer = customerRepository.save(customer);
+      
+      if (savedCustomer.getId() > 0) {
+        response = ResponseEntity
+          .status(HttpStatus.CREATED)
+          .body("Given user details have been registered for user successfully.");
+      }
+    } catch (Exception e) {
+      response = ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("An exception occurred due to " + e.getMessage());
+    }
+    
+    return response;
+
+  }
+
+}
---
+++ eazybank-webapp/section6-cors-csrf/src/main/java/com/learnspringsec/eazybank/controller/NoticesController.java
@@ -0,0 +1,14 @@
+package com.learnspringsec.eazybank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+  
+  @GetMapping("/notices")
+  public String getAccountDetails() {
+    return "Notices details are saved to the DB";
+  }
+
+}
