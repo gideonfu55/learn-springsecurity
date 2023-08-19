7256e58ad54b3c196b59fcbf46bbb03fa5b8e595
Gideon Fu <fuzhonglong@gmail.com>
2023-08-19 23:50:55 +0800

    add global CORS configuration in Security Config

--- eazybank-webapp/section6-cors-csrf/src/main/java/com/learnspringsec/eazybank/config/ProjectSecurityConfig.java
+++ eazybank-webapp/section6-cors-csrf/src/main/java/com/learnspringsec/eazybank/config/ProjectSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.learnspringsec.eazybank.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +9,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -14,14 +20,27 @@ public class ProjectSecurityConfig {
   // Basic structure for customizing your Security Configuration:
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http
-      .csrf(csrf -> csrf.disable())
-      .authorizeHttpRequests((requests) -> requests
-        .requestMatchers("/myAccount", "myBalance", "/myLoans", "/myCards", "/user").authenticated()
-        .requestMatchers("notices", "/contact", "/register").permitAll()
-      )
-      .formLogin(Customizer.withDefaults())
-      .httpBasic(Customizer.withDefaults());
+      http
+        // Addition of CORs configuration:
+        .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+          @Override
+          public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowCredentials(true);
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setMaxAge(3600L);
+            return config;
+          }
+        }))
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests((requests) -> requests
+          .requestMatchers("/myAccount", "myBalance", "/myLoans", "/myCards", "/user").authenticated()
+          .requestMatchers("notices", "/contact", "/register").permitAll()
+        )
+        .formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults());
 
     return http.build();
   }
