1487656c561a663cdfae056c32c6ebee4f555772
Gideon Fu <fuzhonglong@gmail.com>
2023-08-19 19:57:35 +0800

    add /user to request authorization in Security Config

--- eazybank-webapp/section6-cors-csrf/src/main/java/com/learnspringsec/eazybank/config/ProjectSecurityConfig.java
+++ eazybank-webapp/section6-cors-csrf/src/main/java/com/learnspringsec/eazybank/config/ProjectSecurityConfig.java
@@ -17,7 +17,7 @@ public class ProjectSecurityConfig {
     http
       .csrf(csrf -> csrf.disable())
       .authorizeHttpRequests((requests) -> requests
-        .requestMatchers("/myAccount", "myBalance", "/myLoans", "/myCards").authenticated()
+        .requestMatchers("/myAccount", "myBalance", "/myLoans", "/myCards", "/user").authenticated()
         .requestMatchers("notices", "/contact", "/register").permitAll()
       )
       .formLogin(Customizer.withDefaults())
