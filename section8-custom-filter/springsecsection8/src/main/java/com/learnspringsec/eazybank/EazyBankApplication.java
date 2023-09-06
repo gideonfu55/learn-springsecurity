0d1b19859a01d5750c7805a09646f6d07b243211
Gideon Fu <fuzhonglong@gmail.com>
2023-09-06 13:19:53 +0800

    add EnableWebSecurity annotation

--- section8-custom-filter/springsecsection8/src/main/java/com/learnspringsec/eazybank/EazyBankApplication.java
+++ section8-custom-filter/springsecsection8/src/main/java/com/learnspringsec/eazybank/EazyBankApplication.java
@@ -2,8 +2,10 @@ package com.learnspringsec.eazybank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class EazyBankApplication {
 
 	public static void main(String[] args) {
